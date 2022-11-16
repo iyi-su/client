429b7e374702f98d553c412b103116605a33d564
semen.kamenetskiy <semen.kamenetskiy@aliexpress.ru>
2022-11-16 13:41:03 +0300

    updates to client

--- client.ts
+++ client.ts
@@ -1,7 +1,9 @@
 import {
     ApolloCache,
-    ApolloClient, FetchResult,
-    InMemoryCache, MutationOptions,
+    ApolloClient,
+    FetchResult,
+    InMemoryCache,
+    MutationOptions,
     NormalizedCacheObject,
     OperationVariables,
     TypedDocumentNode,
@@ -11,18 +13,23 @@ import {ApolloQueryResult} from "@apollo/client/core/types";
 
 export class Client<T extends Client<any>> {
 
-    public static create<T>(address: string, cache: ApolloCache<NormalizedCacheObject> = new InMemoryCache()): T {
+    protected static envVar: string = "CLIENT_ADDRESS";
+
+    public static create<T>(
+        address: string = getAddress(this.envVar),
+        cache: ApolloCache<NormalizedCacheObject> = new InMemoryCache(),
+    ): T {
         return new this(address, cache) as T;
     }
 
     readonly address: string;
-    readonly client: ApolloClient<NormalizedCacheObject>;
-    readonly cache: ApolloCache<NormalizedCacheObject>;
+    readonly #client: ApolloClient<NormalizedCacheObject>;
+    readonly #cache: ApolloCache<NormalizedCacheObject>;
 
     protected constructor(uri: string, cache: ApolloCache<NormalizedCacheObject>) {
         this.address = uri;
-        this.cache = cache;
-        this.client = new ApolloClient<NormalizedCacheObject>({uri, cache});
+        this.#cache = cache;
+        this.#client = new ApolloClient<NormalizedCacheObject>({uri, cache});
     }
 
     protected async query<T = any, TVariables = OperationVariables>(
@@ -36,7 +43,7 @@ export class Client<T extends Client<any>> {
     protected async queryRaw<T = any, TVariables = OperationVariables>(
         options: QueryOptions<TVariables, T>,
     ): Promise<ApolloQueryResult<T>> {
-        return this.client.query(options);
+        return this.#client.query(options);
     }
 
     protected async mutate<TData = any, TVariables = OperationVariables>(
@@ -50,7 +57,15 @@ export class Client<T extends Client<any>> {
     protected async mutateRaw<TData = any, TVariables = OperationVariables>(
         options: MutationOptions<TData, TVariables>,
     ): Promise<FetchResult<TData>> {
-        return this.client.mutate<TData, TVariables>(options);
+        return this.#client.mutate<TData, TVariables>(options);
     }
 
+}
+
+function getAddress(envVar: string): string {
+    const address = process.env[envVar];
+    if (!address) {
+        throw new Error(`${envVar} is not defined`);
+    }
+    return address;
 }
